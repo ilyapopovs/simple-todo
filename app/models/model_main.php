58494620caf7e71472babbdf2d29d03f9c8aaeb1
elias-po <elias.popovs@gmail.com>
2019-06-16 20:53:29 +0300

    check if queried successfully

--- app/models/model_main.php
+++ app/models/model_main.php
@@ -12,16 +12,20 @@ class ModelMain extends Model {
         // Makes a call to the Database
         // Returns an array of existing tasks
 
+        //TODO: check for existing connection instead of reconnecting every time
         $db = new mysqli(DB_LOCATION, DB_USERNAME, DB_PASSWORD, DB_NAME);
         if ($db->connect_error) {
             die("Connection failed: " . $db->connect_error);
         }
 
         $sql = "SELECT * FROM " . DB_TABLE;
-        $result = $db->query($sql);
-        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-            $arrayResponse[] = $row;
+        if($result = $db->query($sql)) {
+            while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                $arrayResponse[] = $row;
+            }
+            $result->free();
         }
+        $db->close();
 
         return $arrayResponse;
     }
