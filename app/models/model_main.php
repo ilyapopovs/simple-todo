21619f306498df37cb04bff8954b3bc442d6a37f
elias-po <elias.popovs@gmail.com>
2019-06-16 18:18:01 +0300

    Main page pulls info from DB now, basic credential hiding

--- app/models/model_main.php
+++ app/models/model_main.php
@@ -9,25 +9,21 @@ class ModelMain extends Model {
     }
 
     public function getAllTasks() {
-        // Supposed to make a call to the Database here
-        // Retruns an array of existing tasks
+        // Makes a call to the Database
+        // Returns an array of existing tasks
 
-        $response_stub = [
-            [
-                "id" => "1",
-                "title" => "Make Structured Base",
-                "description" => "Create a nearly empty, but working base for the project.",
-                "date_added" => date("d.m.Y", mktime(23, 1, 30, 6, 14, 2019)),
-                "done" => true
-            ], [
-                "id" => "2",
-                "title" => "Design The Main Page",
-                "description" => "Make it look good :)",
-                "date_added" => date("d.m.Y", mktime(23, 30, 30, 6, 15, 2019)),
-                "done" => false
-            ]
-        ];
-        return $response_stub;
+        $db = new mysqli(DB_LOCATION, DB_USERNAME, DB_PASSWORD, DB_NAME);
+        if ($db->connect_error) {
+            die("Connection failed: " . $db->connect_error);
+        }
+
+        $sql = "SELECT * FROM " . DB_TABLE;
+        $result = $db->query($sql);
+        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            $arrayResponse[] = $row;
+        }
+
+        return $arrayResponse;
     }
 }
 
--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
     session_start();
+    
+    // Gets DB related info from the config file and defines according constants
+    // If config.ini doesn't exist - uses config_example.ini
+    // WARNING: Insecure way of storing credentials
+    // TODO: hide credentials outside the scope of the project
+    $config = file_exists('./config.ini') ? parse_ini_file('./config.ini') : parse_ini_file('./config_example.ini');
+    defined('DB_LOCATION') || define('DB_LOCATION', $config['dblocation']);
+    defined('DB_USERNAME') || define('DB_USERNAME', $config['username']);
+    defined('DB_PASSWORD') || define('DB_PASSWORD', $config['password']);
+    defined('DB_NAME') || define('DB_NAME', $config['dbname']);
+    defined('DB_TABLE') || define('DB_TABLE', $config['dbtable']);
+
     // attaching parent classes
     require("app/core/model.php");
     require("app/core/controller.php");
@@ -32,7 +44,7 @@
             $controller->{$_GET['action']}();
         }
     }
-    
+
     // showing the page
     echo $view->show();
 
